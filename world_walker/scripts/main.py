3936564b9181b0f01a93c3fd1679bd014d4626bc
Alexander-Zaychenko <alexzay2012@yandex.ru>
2023-07-07 16:42:02 +0300

    Add mine which zero score

--- world_walker/scripts/main.py
+++ world_walker/scripts/main.py
@@ -2,20 +2,44 @@ import pygame
 import random
 
 
+def change_pos_ent(x, y):
+    new_ent_pos = {
+        "x": round(random.randrange(0, x - 10) / 10) * 10,
+        "y": round(random.randrange(0, y - 10) / 10) * 10,
+    }
+    return new_ent_pos
+
+
+def add_ent(display, color, player_pos, player_size):
+    pygame.draw.rect(display, color, [
+        player_pos["x"],
+        player_pos["y"],
+        player_size[0],
+        player_size[1]])
+
+
+def check_collision(player_x, player_y, second_x, second_y, player_size):
+    x_collision = (abs(player_x - second_x) < player_size[0])
+    y_collision = (abs(player_y - second_y) < player_size[1])
+    return x_collision and y_collision
+
+
 def main():
     # create variables
+    fps = 25
     current_frame = 1
     buffing = False
     start_frame_buffing = -250
-    ten_secs_in_frames = 250
+    ten_secs_in_frames = fps * 10
     speed_buff_rate = 3
 
     # initialize game
     pygame.init()
 
     # create display & run update
-    width = 640
-    height = 480
+    width = 1080
+    height = 720
+
     display = pygame.display.set_mode((width, height))
 
     pygame.display.update()
@@ -23,9 +47,10 @@ def main():
 
     # define colors
     colors = {
-        "snake_head": (0, 255, 0),
-        'apple': (255, 0, 0),
-        'speed_buff': (128, 166, 255)
+        "player": (0, 255, 0),
+        'food': (255, 0, 0),
+        'speed_buff': (128, 166, 255),
+        'mine': (255, 255, 0)
     }
 
     # snake position with offsets
@@ -43,20 +68,16 @@ def main():
     player_speed = 2
 
     # food
-    food_pos = {
-        "x": round(random.randrange(0, width - player_size[0]) / 10) * 10,
-        "y": round(random.randrange(0, height - player_size[1]) / 10) * 10,
-    }
+    food_pos = change_pos_ent(width, height)
 
     food_size = (10, 10)
     food_eaten = 0
 
     # speed buffer
-    speed_buff_pos = {
-        "x": round(random.randrange(0, width - player_size[0]) / 10) * 10,
-        "y": round(random.randrange(0, height - player_size[1]) / 10) * 10,
-    }
+    speed_buff_pos = change_pos_ent(width, height)
 
+    # mine
+    mine_pos = change_pos_ent(width, height)
     speed_buff_size = (10, 10)
 
     # start loop
@@ -72,22 +93,22 @@ def main():
                 game_end = True
 
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT or event.key == ord('a'):
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
                     # move left
                     player_pos["x_change"] = -player_speed
                     player_pos["y_change"] = 0
 
-                elif event.key == pygame.K_RIGHT or event.key == ord('d'):
+                elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                     # move right
                     player_pos["x_change"] = player_speed
                     player_pos["y_change"] = 0
 
-                elif event.key == pygame.K_UP or event.key == ord('w'):
+                elif event.key == pygame.K_UP or event.key == pygame.K_w:
                     # move up
                     player_pos["x_change"] = 0
                     player_pos["y_change"] = -player_speed
 
-                elif event.key == pygame.K_DOWN or event.key == ord('s'):
+                elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
                     # move down
                     player_pos["x_change"] = 0
                     player_pos["y_change"] = player_speed
@@ -112,54 +133,49 @@ def main():
         elif player_pos["y"] > height:
             player_pos["y"] = 0
 
-        pygame.draw.rect(display, colors["snake_head"], [
-            player_pos["x"],
-            player_pos["y"],
-            player_size[0],
-            player_size[1]])
+        # draw player
+        add_ent(display, colors['player'], player_pos, player_size)
 
         # draw food
-        pygame.draw.rect(display, colors["apple"], [
-            food_pos["x"],
-            food_pos["y"],
-            food_size[0],
-            food_size[1]])
+        add_ent(display, colors['food'], food_pos, player_size)
 
         # draw speed buff
-        pygame.draw.rect(display, colors['speed_buff'], [
-            speed_buff_pos["x"],
-            speed_buff_pos["y"],
-            speed_buff_size[0],
-            speed_buff_size[1]])
+        add_ent(display, colors['speed_buff'], speed_buff_pos, player_size)
+
+        # draw mine
+        add_ent(display, colors['mine'], mine_pos, player_size)
 
         # detect collision with food
-        if (abs(player_pos["x"] - food_pos["x"]) < 10) and (abs(player_pos["y"] - food_pos["y"]) < 10):
+        if check_collision(player_pos['x'], player_pos['y'], food_pos['x'], food_pos['y'], player_size):
             food_eaten += 1
 
-            food_pos = {
-                "x": round(random.randrange(0, width - player_size[0]) / 10) * 10,
-                "y": round(random.randrange(0, height - player_size[1]) / 10) * 10,
-            }
+            food_pos = change_pos_ent(width, height)
 
-        # detect collision with speed buff and buff player, if required
-        if (abs(player_pos["x"] - speed_buff_pos["x"]) < 10) and (abs(player_pos["y"] - speed_buff_pos["y"]) < 10):
-            speed_buff_pos = {
-                "x": round(random.randrange(0, width - player_size[0]) / 10) * 10,
-                "y": round(random.randrange(0, height - player_size[1]) / 10) * 10,
-            }
+            print(f"Очков: {food_eaten}")
 
+        # detect collision with speed buff
+        if check_collision(player_pos['x'], player_pos['y'], speed_buff_pos['x'], speed_buff_pos['y'], player_size):
             if not buffing:
                 buffing = True
                 start_frame_buffing = current_frame
                 player_speed *= speed_buff_rate
+
+                speed_buff_pos = change_pos_ent(width, height)
+
         if buffing and current_frame - start_frame_buffing > ten_secs_in_frames:
             player_speed /= speed_buff_rate
             buffing = False
 
+        # detect collision with mine
+        if check_collision(player_pos['x'], player_pos['y'], mine_pos['x'], mine_pos['y'], player_size):
+            print('Очки сброшены!')
+            food_eaten = 0
+            mine_pos = change_pos_ent(width, height)
+
         pygame.display.update()
 
         # set FPS
-        clock.tick(25)
+        clock.tick(fps)
         current_frame += 1
 
     # close app, if required
